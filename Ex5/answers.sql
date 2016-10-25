49abac0b9ccfc98dc7eb942bd1379a78a57b5b55
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-10-25 14:43:43 +0100

    Ex5 Redone

--- Ex5/answers.sql
+++ Ex5/answers.sql
@@ -2,29 +2,37 @@
 SELECT DISTINCT City FROM Email;
 
 --How many people responded from each city
-SELECT City , COUNT(*) 
+SELECT City , COUNT(*) AS Responses
 FROM Email 
-GROUP BY City;
+GROUP BY City
+HAVING COUNT(*) = 
+(
+    SELECT COUNT(*) AS counts  
+    FROM Email 
+    GROUP BY City
+    ORDER BY COUNT(*) DESC
+    LIMIT 1
+);
 
 --Which city were the maximum respondents from?
-select City 
+SELECT City 
 FROM Email 
 GROUP BY City 
 ORDER BY count(*) DESC 
 LIMIT 1;
 
 --What all email domains did people respond from ?
-SELECT DISTINCT SUBSTRING_INDEX(Email,'@',-1)
+SELECT DISTINCT SUBSTRING_INDEX(Email,'@',-1) AS 'Email Domain'
 FROM Email;
 
 --Which is the most popular email domain among the respondents ?
-SELECT DISTINCT SUBSTRING_INDEX(Email,'@',-1) , COUNT(*) 
+SELECT SUBSTRING_INDEX(Email,'@',-1) AS 'Email Domain', COUNT(*) AS Frequency
 FROM Email 
 GROUP BY SUBSTRING_INDEX(Email, '@', -1)
 HAVING COUNT(*) = 
 (
     SELECT COUNT(*) AS counts  
     FROM Email GROUP BY substring_index(Email, '@', -1)
-    ORDER BY count(*) DESC
+    ORDER BY COUNT(*) DESC
     LIMIT 1
 );
